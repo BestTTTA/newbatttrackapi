1cff7aa8393216c9675aa2d767d2795685555f54
BestTTTA <bst12546@gmail.com>
2024-02-20 15:47:28 +0700

    update

--- employee_routers.py
+++ employee_routers.py
@@ -83,12 +83,14 @@ async def update_employee_current_stage(product_id: str, update_data: EmployeeSt
 @router.put("/add_employee/{product_id}")
 async def add_employee_to_product(product_id: str, employee: Employee = Body(...)):
     product = db.products.find_one({"product_id": product_id})
+    
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
-    
-    # if any(emp['user_id'] == employee.user_id for emp in product.get("employees", [])):
-    #     raise HTTPException(status_code=400, detail="Employee already added")
-    
+
+    # Check if an employee with the same name and current_stage is already added to the product
+    if any(emp['name'] == employee.name and emp['current_stage'] == employee.current_stage for emp in product.get("employees", [])):
+        raise HTTPException(status_code=400, detail="Employee with the same name and current stage already added")
+
     db.products.update_one({"product_id": product_id}, {"$push": {"employees": employee.dict()}})
     return {"message": "Employee added successfully to product"}
 
